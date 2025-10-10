d262bcbbac8a49b9171140ccd99ab8b612c0d60b
Selamu Dawit <selamudev@gmail.com>
2025-10-10 18:42:45 +0300

    return back to normal auth

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -11,23 +11,23 @@ function isExpired(token) {
 
 export default defineNuxtRouteMiddleware(async (to) => {
   console.log("🔍 Checking route:", to.path);
-  // const { getToken } = useApollo();
-  // const token = await getToken("auth");
-  // const userData = useCookie("userData");
-  // console.log("🔍 Checking route:", to.path);
-  // console.log("🔹 Token:", token);
-  // console.log("🔹 User Data:", userData.value);
-  // // "/" route: if user is logged in → redirect to /lotteries
-  // if (to.path === "/" && token && userData.value && !isExpired(token)) {
-  //   console.log("✅ User already logged in — redirecting to /lotteries");
-  //   return navigateTo("/lotteries");
-  // }
-  // // Other routes: validate token + user
-  // if (to.path !== "/") {
-  //   if (!token || !userData.value || isExpired(token)) {
-  //     console.warn("🚫 Unauthorized — redirecting to /");
-  //     return navigateTo("/");
-  //   }
-  // }
-  // console.log("✅ Authorized access:", to.path);
+  const { getToken } = useApollo();
+  const token = await getToken("auth");
+  const userData = useCookie("userData");
+  console.log("🔍 Checking route:", to.path);
+  console.log("🔹 Token:", token);
+  console.log("🔹 User Data:", userData.value);
+  // "/" route: if user is logged in → redirect to /lotteries
+  if (to.path === "/" && token && userData.value && !isExpired(token)) {
+    console.log("✅ User already logged in — redirecting to /lotteries");
+    return navigateTo("/lotteries");
+  }
+  // Other routes: validate token + user
+  if (to.path !== "/") {
+    if (!token || !userData.value || isExpired(token)) {
+      console.warn("🚫 Unauthorized — redirecting to /");
+      return navigateTo("/");
+    }
+  }
+  console.log("✅ Authorized access:", to.path);
 });
