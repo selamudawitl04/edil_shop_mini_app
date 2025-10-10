5c8313a6ea7e331c1d4f309a11ad6eaa07b3357b
Selamu Dawit <selamudev@gmail.com>
2025-10-10 18:56:00 +0300

    back to test

--- app.vue
+++ app.vue
@@ -24,19 +24,19 @@ const { onLogin, getToken } = useApollo();
 //   avatar_color: "#4A90E2",
 // };
 
-// onLogin(
-//   "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS5lZGlsc2hvZi5jb20iLCJlbnRpdHlfaWQiOiIiLCJleHAiOjE3NjA3MTAzODIsImlhdCI6MTc2MDEwNTU4MiwiaXNzIjoiaHR0cHM6Ly9hcGkuZWRpbHNob2YuY29tIiwibWV0YWRhdGEiOnsibmFtZSI6IlNhbXVlbCBOb2FoIiwicm9sZXMiOlsidXNlciJdfSwicm9sZSI6WyJ1c2VyIl0sInN1YiI6IjIxMmI1OTBjLTRlOTYtNGUxZi04Y2MwLWJmODIzMTE5NDQ1ZiJ9.M_mmodFqIrwQjeToeQklOZC_PLy2DYugxm37r6Oxp00cjNvYuwkXkXZkUEH7SLBFMoOOJZxyEhdZlSMijtf-CA",
-//   "auth"
-// );
+onLogin(
+  "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS5lZGlsc2hvZi5jb20iLCJlbnRpdHlfaWQiOiIiLCJleHAiOjE3NjA3MTAzODIsImlhdCI6MTc2MDEwNTU4MiwiaXNzIjoiaHR0cHM6Ly9hcGkuZWRpbHNob2YuY29tIiwibWV0YWRhdGEiOnsibmFtZSI6IlNhbXVlbCBOb2FoIiwicm9sZXMiOlsidXNlciJdfSwicm9sZSI6WyJ1c2VyIl0sInN1YiI6IjIxMmI1OTBjLTRlOTYtNGUxZi04Y2MwLWJmODIzMTE5NDQ1ZiJ9.M_mmodFqIrwQjeToeQklOZC_PLy2DYugxm37r6Oxp00cjNvYuwkXkXZkUEH7SLBFMoOOJZxyEhdZlSMijtf-CA",
+  "auth"
+);
 
-// useCookie("userData").value = {
-//   id: "212b590c-4e96-4e1f-8cc0-bf823119445f",
-//   name: "Samuel Noah",
-//   phone: "0945003939",
-//   alternate_phone: null,
-//   role: "user",
-//   is_phone_verified: true,
-//   profile_image: "",
-//   avatar_color: "#4DD0E1",
-// };
+useCookie("userData").value = {
+  id: "212b590c-4e96-4e1f-8cc0-bf823119445f",
+  name: "Test User",
+  phone: "0945003939",
+  alternate_phone: null,
+  role: "user",
+  is_phone_verified: true,
+  profile_image: "",
+  avatar_color: "#4DD0E1",
+};
 </script>
