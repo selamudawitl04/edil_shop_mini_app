6c9ada83389190c3a3087225030d606da033b5b6
Selamu Dawit <selamudev@gmail.com>
2025-10-18 10:46:24 +0300

    feat: add PWA support with manifest and app icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,9 @@ export default defineNuxtConfig({
           href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
           rel: "stylesheet",
         },
+
+        { rel: "manifest", href: "/manifest.json" },
+        { rel: "apple-touch-icon", href: "/logo-192.png" },
       ],
       script: [
         {
