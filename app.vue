cf840158c7db513d73693d2d2ae03c1b7141a1ce
Selamu Dawit <selamudev@gmail.com>
2025-10-31 07:31:13 +0300

    fix login

--- app.vue
+++ app.vue
@@ -7,8 +7,9 @@
 <script setup>
 import "vue-toast-notification/dist/theme-sugar.css";
 const { onLogin, getToken, onLogout } = useApollo();
-
-// import getUserQuery from "@/graphql/auth/user_item.gql";
+const router = useRouter();
+const route = useRoute();
+import getUserQuery from "@/graphql/auth/user_item.gql";
 
 // onLogin(
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS5lZGlsc2hvcC5jb20iLCJlbnRpdHlfaWQiOiIiLCJleHAiOjE3NjIyMzc0OTYsImlhdCI6MTc2MTYzMjY5NiwiaXNzIjoiaHR0cHM6Ly9hcGkuZWRpbHNob3AuY29tIiwibWV0YWRhdGEiOnsibmFtZSI6IlNhbXVlbCBOZXciLCJyb2xlcyI6WyJ1c2VyIl19LCJyb2xlIjpbInVzZXIiXSwic3ViIjoiOWIxMmMyZDctYTRkOC00MDY3LTgwNzgtZjgxOWFhYzE0MmRjIn0.7JdcuCROBwz7EneBuAZXGsaug77j3k3F4AFmpC4JnII",
@@ -26,9 +27,6 @@ const { onLogin, getToken, onLogout } = useApollo();
 //   avatar_color: "#4A90E2",
 // };
 
-const router = useRouter();
-const route = useRoute();
-
 const checkTelegramAuth = async () => {
   const userData = useCookie("userData");
   const tg = window?.Telegram?.WebApp;
@@ -37,7 +35,7 @@ const checkTelegramAuth = async () => {
   const telegramUserId = tg.initDataUnsafe?.user?.id;
   let storedUserId = userData.value?.telegram_user_id;
 
-  alert(telegramUserId + " Telegram User ID");
+  alert(telegramUserId + " Telegram User ID", userData.value.id + " User ID");
 
   if (userData.value) {
     const { onResult, onError } = queryItem(getUserQuery, {
